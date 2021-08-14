c8b552068ac400dc95c19e234b3dc5949de02aa7
XxWildHunterxX <tanjunhao777@gmail.com>
2021-08-14 15:25:10 +0800

    TanJunHaoCommit

---
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/entity/createDB.kt
@@ -0,0 +1,12 @@
+package com.junhao.hetty_computer_warehouse_system.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class User(
+
+@PrimaryKey val uid:Int
+
+)
