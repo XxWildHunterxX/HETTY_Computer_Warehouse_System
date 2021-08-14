9d187b2a10af8e9f5ebece18efcdb91aae844da3
henrycjl-wm18@student.tarc.edu.my <henrycjl-wm18@student.tarc.edu.my>
2021-08-14 21:52:23 +0800

    henry

---
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/ui/fragment_addsales.kt
@@ -0,0 +1,22 @@
+package com.junhao.hetty_computer_warehouse_system.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.junhao.hetty_computer_warehouse_system.R
+
+class fragment_addsales : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_addsales, container, false)
+    }
+
+
+}
