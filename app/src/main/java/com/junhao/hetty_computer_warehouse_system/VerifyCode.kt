3a2362fc1d0563b1cfff55dde8898cae1cab29ff
yewjunli21 <yewjunli21@gmail.com>
2021-08-15 22:28:15 +0800

    Junli commit

---
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/VerifyCode.kt
@@ -0,0 +1,11 @@
+package com.junhao.hetty_computer_warehouse_system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifyCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_code)
+    }
+}
