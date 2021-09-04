7b35eea10746443bc9431e4053699cdff22f50de
yewjunli <yewjunli21@gmail.com>
2021-09-04 16:47:46 +0800

    Junli commit

--- app/src/main/java/com/junhao/hetty_computer_warehouse_system/fragment_addStaff.kt
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/fragment_addStaff.kt
@@ -5,15 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+
+import com.google.firebase.database.FirebaseDatabase
+import com.junhao.hetty_computer_warehouse_system.data.Staff
+
 
 class fragment_addStaff : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_staff, container, false)
-    }
+
 
 }
