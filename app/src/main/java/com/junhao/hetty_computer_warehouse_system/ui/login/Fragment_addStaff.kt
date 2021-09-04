3f470318b72740b5c4947a572102a3265dd47e7c
yewjunli <yewjunli21@gmail.com>
2021-09-04 21:22:56 +0800

    Junli commit

--- app/src/main/java/com/junhao/hetty_computer_warehouse_system/ui/login/Fragment_addStaff.kt
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/ui/login/Fragment_addStaff.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.database.FirebaseDatabase
 import com.junhao.hetty_computer_warehouse_system.R
+import com.junhao.hetty_computer_warehouse_system.data.Staff
 
 class Fragment_addStaff : Fragment() {
 
@@ -13,8 +16,21 @@ class Fragment_addStaff : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_add_staff, container, false)
+
+        view.findViewById<Button>(R.id.btnAddStaff).setOnClickListener{
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("Staff")
+
+            val staff = Staff("S001","Jun Hao","Male")
+
+            myRef.child(staff.id).setValue(staff)
+        }
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_staff, container, false)
+        return view
     }
 
 }
