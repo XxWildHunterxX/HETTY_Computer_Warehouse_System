2a29679792991c00f16ccef2d5c40c609fa1de1d
XxWildHunterxX <tanjunhao777@gmail.com>
2021-09-05 23:13:26 +0800

    jh commit

--- app/src/main/java/com/junhao/hetty_computer_warehouse_system/data/Product.kt
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/data/Product.kt
@@ -1,11 +1,11 @@
 package com.junhao.hetty_computer_warehouse_system.data
 
 data class Product(
-    val id :String,
     val name :String,
     val productBarcode :String,
     val productRack : String,
     val productType:String,
     val productDescription : String,
-    val minimumAlertQty : String
+    val minimumAlertQty : String,
+    val alertChk :String
     )
