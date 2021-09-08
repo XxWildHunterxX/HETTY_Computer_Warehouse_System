e5c2a6367b203ed7fef7efb7e8ce155662719069
YewJunLi <yewjunli21@gmail.com>
2021-09-08 23:28:27 +0800

    Jun Li commit

--- app/src/main/java/com/junhao/hetty_computer_warehouse_system/data/Staff.kt
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/data/Staff.kt
@@ -1,14 +1,14 @@
 package com.junhao.hetty_computer_warehouse_system.data
 
 data class Staff(
-    val id :String,
-    val name :String,
-    val gender :String,
-    val dateOfBirth :String,
-    val address :String,
-    val phoneNum :String,
-    val email :String,
-    val position :String,
-    val joinDate :String,
-    val staffImg :String
+    val id :String?=null,
+    val name :String?=null,
+    val gender :String?=null,
+    val dateOfBirth :String?=null,
+    val address :String?=null,
+    val phoneNum :String?=null,
+    val email :String?=null,
+    val position :String?=null,
+    val joinDate :String?=null,
+    val staffImg :String?=null
 )
