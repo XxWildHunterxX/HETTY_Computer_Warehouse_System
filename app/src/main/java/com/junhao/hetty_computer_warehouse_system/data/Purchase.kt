3d9ff7dfc1744250c58ee7324feb1a2a1880df31
venfang <ewevenfang@gmail.com>
2021-09-23 06:17:37 +0800

    VF 9/23

--- app/src/main/java/com/junhao/hetty_computer_warehouse_system/data/Purchase.kt
+++ app/src/main/java/com/junhao/hetty_computer_warehouse_system/data/Purchase.kt
@@ -3,7 +3,7 @@ package com.junhao.hetty_computer_warehouse_system.data
 data class Purchase(
     val purchaseID :String?="",
     val purProductName: String?="",
-    val purQty: String?="" ,
+    val purQty: String?="",
     val costPerUnit : String?="",
     val totalCost:String?="",
     val supplierName:String?="",
@@ -13,6 +13,6 @@ data class Purchase(
     val acceptDate:String?="",
     val rejectDate:String?="",
     val deliverDate:String?="",
-    val receivedDate:String?="",
-    val status:String?=""
+    var receivedDate:String?="",
+    var status:String?=""
 )
